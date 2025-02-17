2933ad69cca62543513195c1d1bbd4929304c816
eduardo-rbf <eduardofilho@aluno.utfpr.edu.br>
2025-02-17 13:11:44 -0300

    Pre-merge, updated player spawn, player movement logic, added levels, added level logic(proof of concept implementation on test level), working on level generics vs. specific level code

--- scripts/levels/level.gd
+++ scripts/levels/level.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 class_name Level
 
+##Refer to TestLevel's LevelScript for documentation.
+
 const _DIALOG_SCREEN: PackedScene = preload("res://scenes/dialog/dialog.tscn")
+
+@onready var Player = $Player
+@onready var Map = $Map
+@onready var Commands = $Commands
 
 var _dialog_data: Dictionary = {
 	0: {
@@ -24,10 +30,12 @@ var _dialog_data: Dictionary = {
 func _ready() -> void:
 
 	$"Fade Transiction".fade_in()
+	Player.map = Map
+	Commands.Player = Player
 
-	var _new_dialog: DialogScreen = _DIALOG_SCREEN.instantiate()
-	_new_dialog.data = _dialog_data
-	_hud.add_child(_new_dialog)
+	#var _new_dialog: DialogScreen = _DIALOG_SCREEN.instantiate()
+	#_new_dialog.data = _dialog_data
+	#_hud.add_child(_new_dialog)
 
 func level_reset():
 	pass
